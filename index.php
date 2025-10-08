77447018c7c76b456105c3126c9226f37fc01e17
mariano-a-n <nescimarianoa999@gmail.com>
2025-10-08 14:00:01 -0300

    cambios simples  en el index 8/10/25

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-    echo 'TPE1';
-    echo 'fin';
-    echo '12345';
-    echo "hola";
-    echo 'que tal';
+require_once "tareasPDO.php";
+
+        require_once "./templates/header.php";
+
+            listaMarcas();
+
+            require_once "./templates/footer.php";
+    
+    
 ?>
