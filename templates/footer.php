af283f3d7250560e4b46118ba80c4b078693911c
mariano-a-n <nescimarianoa999@gmail.com>
2025-10-08 14:24:09 -0300

    cambios de los templates 08/10/25

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+
+    <footer>
+        <p>esto es el footer</p>
+        <div>
+            <h3 class="subtitulo_footer">dueños de la paguina</h3>
+            <p class="p_creadores"><a href=""></a>mariano nesci</p>
+            <p class="p_creadores"><a href=""></a>marcelo galato</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1 class="titulo">concesionaria M&M</h1>
+        <nav class="varra_de_tareas ">
+            <ul>
+                <li><a href="home">inicio</a></li>
+            </ul>
+        </nav>
+    </header>
