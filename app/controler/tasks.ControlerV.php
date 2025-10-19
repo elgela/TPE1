03fe3a10b46157718a8173db53899e4f41736f06
Marcelo Gelato <gelatomarcelo@hotmail.com>
2025-10-19 18:49:12 -0300

    eraseCar() y acomodos en addCarModel() en variables

--- app/controler/tasks.ControlerV.php
+++ app/controler/tasks.ControlerV.php
@@ -19,13 +19,18 @@
 
             //actualizo la vista
             $this->veiw->showTaksV($modelos);
-
         }
 
         function sellCar($id) {
             $this->model->updateCar($id);
             header("Location: " . BASE_URL . "modelos");
         }
+
+        function eraseCar($id) {
+            $this->model->deleteCar($id);
+            header("Location: " . BASE_URL . "modelos");
+        }
+
 //////////////////
         function showCarDetails($id) {
             // obtengo los modelos de vehiculos
@@ -49,31 +54,37 @@
             $modelos = $this->model->getCarModel();
 
             //actualizo la vista
-            $this->veiw->factoryCars($modelos, $id);
+            $this->veiw->yesNewCars($modelos, $id);
         }
 
-        
         function addCarModel() {
-            $this->veiw->addCar();
-            if (isset($_POST['modelo'], $_POST['anio'], $_POST['km'], $_POST['precio'], $_POST['patente'], $_POST['imagen'], $_POST['id_marca'])) {
+            if (isset($_POST['modelo'], $_POST['anio'], $_POST['km'], $_POST['precio'], $_POST['patente'], $_POST['marca']) && !empty($_POST['marca']) && !empty($_POST['modelo']) && !empty($_POST['anio']) && !empty($_POST['km']) && !empty($_POST['precio'])) {
                 $modelo = trim($_POST['modelo']); // trim evita espacios accidentales
                 $anio = filter_var($_POST['anio'], FILTER_VALIDATE_INT);
                 $km = filter_var($_POST['km'], FILTER_VALIDATE_INT);
                 $precio = filter_var($_POST['precio'], FILTER_VALIDATE_FLOAT);
                 $patente = strtoupper(trim($_POST['patente'])); // Ej: abc123 -> ABC123
-                $imagen = $_POST['imagen'];
-                $id_marca = $_POST['id_marca'];            
-                $this->model->insertCar($modelo, $anio, $km, $precio, $patente, $imagen, $id_marca);
+                $es_nuevo = isset($_POST['es_nuevo']) ? 1 : 0;
+                $imagen = $_FILES['imagen']['name'];
+                $vendido = isset($_POST['vendido']) ? 1 : 0;
+                $marca = trim($_POST['marca']);
+                $nacionalidad = $_POST['nacionalidad'] ?? null;
+                $anio_de_creacion = $_POST['anio_de_creacion'] ?? null;
+                move_uploaded_file($_FILES['imagen']['tmp_name'], 'img/uploads/' . $_FILES['imagen']['name']);
+                // var_dump($marca, $modelo, $anio, $km, $precio, $patente, $imagen);
+                // die();
+                // echo 'estoy en addcar controler';
+                $this->model->insertCar($modelo, $anio, $km, $precio, $patente, $es_nuevo, $imagen, $vendido, $marca, $nacionalidad, $anio_de_creacion);
+                header("Location: " . BASE_URL . "modelos");
             } else {
                 $this->veiw->showError('Falta ingresar datos');
             }
-
-        //    if (empty($marca) && empty($modelo)) {
-        //         $this->veiw->showError('Falta ingresar datos');
-        //    } else {
-        //    }
         }
-
+        
     }
+    //    if (empty($marca) && empty($modelo)) {
+    //         $this->veiw->showError('Falta ingresar datos');
+    //    } else {
+    //    }
 
 ?>
