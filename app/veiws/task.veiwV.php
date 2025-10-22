26aec1dc4ef8e47051503e09ef3dee17762d2695
Marcelo Gelato <gelatomarcelo@hotmail.com>
2025-10-22 01:00:33 -0300

    addCar(no funciona) agregado

--- app/veiws/task.veiwV.php
+++ app/veiws/task.veiwV.php
@@ -134,60 +134,66 @@ class taskveiwV {
         echo "<h1> Error </h1>";
         echo "<h2> $msj </h2>";
     }
+
+    function addCar() {
+        require_once 'templates/header.phtml';
+        require_once 'templates/form-addCar.phtml';
+        require_once 'templates/footer.phtml';
+    }
     ///////////////mariano-dev
 
-            function showTaksVehiculosUser($modelos , $user) {
-            require_once 'templates/header.phtml';
-            ?>
-            <div class="modelos">
-                    <?php
-                    foreach ($modelos as $modelo) {
-                    ?>
-                    <section>
-                        <h4><?php echo $modelo->modelo ?></h4>
-                        <a href="ver"><img src="<?php echo $modelo->imagen ?>" alt="imagen del modelo"></a>
-                        <h6><?php echo 'Modelo ' . $modelo->anio ?></h6>
-                        <h6><?php echo $modelo->patente ?></h6>
-                        <h5><?php echo number_format($modelo->km, 0, '.', '.') . ' km.' ?></h5>
-                        <h2><?php echo '$ ' . number_format($modelo->precio, 0, ',', '.'); ?></h2>
-                        <?php if ($user) { ?> 
-                            <?php if (!$modelo->vendido) { ?><a href="vendido/<?php echo $modelo->id ?>" type="button" name="comprar" class="btn btn-danger">Comprar</a> <?php } if ($modelo->vendido) echo '<h1>¡Vendido!</h1>' ?>
-                        <?php } ?>
-                        <br>
-                        <br>
-                    </section>
-                    <?php } ?>
-                </div>
-            </form>
+    function showTaksVehiculosUser($modelos , $user) {
+    require_once 'templates/header.phtml';
+    ?>
+    <div class="modelos">
             <?php
-            require_once 'templates/footer.phtml';
-        }
+            foreach ($modelos as $modelo) {
+            ?>
+            <section>
+                <h4><?php echo $modelo->modelo ?></h4>
+                <a href="ver"><img src="<?php echo $modelo->imagen ?>" alt="imagen del modelo"></a>
+                <h6><?php echo 'Modelo ' . $modelo->anio ?></h6>
+                <h6><?php echo $modelo->patente ?></h6>
+                <h5><?php echo number_format($modelo->km, 0, '.', '.') . ' km.' ?></h5>
+                <h2><?php echo '$ ' . number_format($modelo->precio, 0, ',', '.'); ?></h2>
+                <?php if ($user) { ?> 
+                    <?php if (!$modelo->vendido) { ?><a href="vendido/<?php echo $modelo->id ?>" type="button" name="comprar" class="btn btn-danger">Comprar</a> <?php } if ($modelo->vendido) echo '<h1>¡Vendido!</h1>' ?>
+                <?php } ?>
+                <br>
+                <br>
+            </section>
+            <?php } ?>
+        </div>
+    </form>
+    <?php
+    require_once 'templates/footer.phtml';
+    }
 
-        function showModelosPorMarca($modelos) {
-            require_once 'templates/header.phtml';
-            ?>
-            <a href="marcas" class="btn btn-secondary">Volver a marcas</a>
-                <div class="modelos">
-                    <?php
-                    foreach ($modelos as $modelo) {
-                    ?>
-                    <section>
-                        <h4><?php echo $modelo->modelo ?></h4>
-                        <a href="ver"><img src="<?php echo $modelo->imagen ?>" alt="imagen del modelo"></a>
-                        <h6><?php echo 'Modelo ' . $modelo->anio ?></h6>
-                        <h6><?php echo $modelo->patente ?></h6>
-                        <h5><?php echo number_format($modelo->km, 0, '.', '.') . ' km.' ?></h5>
-                        <h2><?php echo '$ ' . number_format($modelo->precio, 0, ',', '.'); ?></h2>
-                        <?php if (!$modelo->vendido) { ?><a href="vendido/<?php echo $modelo->id ?>" type="button" name="comprar" class="btn btn-danger">Comprar</a> <?php } if ($modelo->vendido) echo '<h1>¡Vendido!</h1>' ?>
-                        <br>
-                        <br>
-                    </section>
-                    <?php }
-                    ?>
-                </div>
+    function showModelosPorMarca($modelos) {
+    require_once 'templates/header.phtml';
+    ?>
+    <a href="marcas" class="btn btn-secondary">Volver a marcas</a>
+        <div class="modelos">
             <?php
-            
-        }
+            foreach ($modelos as $modelo) {
+            ?>
+            <section>
+                <h4><?php echo $modelo->modelo ?></h4>
+                <a href="ver"><img src="<?php echo $modelo->imagen ?>" alt="imagen del modelo"></a>
+                <h6><?php echo 'Modelo ' . $modelo->anio ?></h6>
+                <h6><?php echo $modelo->patente ?></h6>
+                <h5><?php echo number_format($modelo->km, 0, '.', '.') . ' km.' ?></h5>
+                <h2><?php echo '$ ' . number_format($modelo->precio, 0, ',', '.'); ?></h2>
+                <?php if (!$modelo->vendido) { ?><a href="vendido/<?php echo $modelo->id ?>" type="button" name="comprar" class="btn btn-danger">Comprar</a> <?php } if ($modelo->vendido) echo '<h1>¡Vendido!</h1>' ?>
+                <br>
+                <br>
+            </section>
+            <?php }
+            ?>
+        </div>
+    <?php
+
+    }
 }
 
 ?>
